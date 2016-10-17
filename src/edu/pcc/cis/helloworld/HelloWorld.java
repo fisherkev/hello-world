759f9c34610007265b451c8d1c850980a5fb6cfd
Kevin Fisher <kevin.fisher15@pcc.edu>
2016-10-17 13:25:43 -0700

    added main method thst prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Kevykev on 10/17/2016.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
